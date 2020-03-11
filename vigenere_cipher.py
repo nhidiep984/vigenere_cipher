6b575c6c928ea03590e77aa022ab07bafda956e0
Nhi <nhidiep@protonmail.com>
2020-03-11 10:18:03 -0700

    finished cipher

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -1,8 +1,6 @@
-alphabet = 'abcdefghijklmnpoqrstuvwxyz'
+from string import ascii_lowercase
 
 def key_generation(plaintext, key_string):
-    #key_string = list(key_string)
-    #keystring_len = len(key_string)
     plaintext_len = len(plaintext)
     repeated_key = key_string
     repeatedkey_len = len(key_string)
@@ -12,18 +10,50 @@ def key_generation(plaintext, key_string):
         repeatedkey_len = len(repeated_key)
         leftover_chars = repeatedkey_len - plaintext_len
         repeated_key = repeated_key[0:repeatedkey_len - leftover_chars]
-    print(''.join(repeated_key))
+    return ''.join(repeated_key)
 
-def encryption():
-    for letter in plaintext:
+def encryption(plaintext, repeated_key):
+    plaintext_pos = [letter_to_index[letter] for letter in plaintext]
+    key_pos = [letter_to_index[letter] for letter in repeated_key]
+    ciphertext_pos = []
+    ciphertext = []
 
+    for x, y in zip(plaintext_pos, key_pos):
+        sum = x+y
+        if sum >= 26:
+            sum = sum - 26
+        ciphertext_pos.append(sum)
 
+    for num in ciphertext_pos:
+        letter = index_to_letter[num]
+        ciphertext.append(letter)
+    return ''.join(ciphertext)
 
 def decryption(ciphertext, repeated_key):
-    pass
+    ciphertext_pos = [letter_to_index[letter] for letter in ciphertext]
+    key_pos = [letter_to_index[letter] for letter in repeated_key]
+    plaintext_pos = []
+    plaintext = []
+
+    for x,y in zip(ciphertext_pos, key_pos):
+        difference = x - y
+        if difference <= 0:
+            difference = difference + 26
+        plaintext_pos.append(difference)
+
+    for num in plaintext_pos:
+        letter = index_to_letter[num]
+        plaintext.append(letter)
+    return ''.join(plaintext)
 
 
 if __name__ == '__main__':
+    letter_to_index = dict(zip(ascii_lowercase, range(len(ascii_lowercase))))
+    index_to_letter = dict(zip(range(len(ascii_lowercase)), ascii_lowercase))
     plaintext = input("Enter string to encrypt: ").lower().replace(" ", "")
     key_string = list(input("Enter key string: "))
-    key_generation(plaintext, key_string)
+    repeated_key = key_generation(plaintext, key_string)
+    ciphertext = encryption(plaintext, repeated_key)
+    print('Ciphertext:', ciphertext)
+    plaintext = decryption(ciphertext, repeated_key)
+    print('Decrypted message: ', plaintext)
